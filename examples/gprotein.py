ae988e27a4c20f4c0b30748dd4d58b82267315ec
Elisa Tonello <elisa.tonello@gmail.com>
2016-09-13 14:35:38 +0100

    Adjusted capitalisation and added use of conservations.

--- examples/gprotein.py
+++ examples/gprotein.py
@@ -4,6 +4,7 @@ sys.path.insert(0, os.path.join(
     os.path.dirname(os.path.realpath(__file__)), '..'))
 
 from crnpy.crn import CRN, from_react_file
+from crnpy.conslaw import ConsLaw
 
 __author__ = "Elisa Tonello"
 __copyright__ = "Copyright (c) 2016, Elisa Tonello"
@@ -16,8 +17,18 @@ __version__ = "0.0.1"
 
 print("Creating model...")
 crn = from_react_file("data/reactions/g-protein_signalling")
-crn.inspect(True)
+crn.inspect(True, invariants = True)
+
+print("")
 
 print("Assuming ligand is constant:")
-crn.remove_constant('l')
-crn.inspect(True)
+crn.remove_constant('L')
+crn.inspect(True, invariants = True)
+
+print("")
+
+print("Using conservations to reduce the number of equations:")
+crn.remove_by_cons('RL', ConsLaw('R + RL', 'Rtot'))
+crn.remove_by_cons('Gbg', ConsLaw('G + Gbg', 'Gtot1'))
+crn.remove_by_cons('Gd', ConsLaw('G + Ga + Gd', 'Gtot'))
+crn.inspect(True, invariants = True)
